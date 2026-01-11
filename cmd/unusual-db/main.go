91930d09f8406da8c60f4982347380dc67ea29d6
Saurabh Sardeshpande <saurabh.ss@gmail.com>
2026-01-10 21:10:07 -0800

    Initial commit for unusual db

---
+++ cmd/unusual-db/main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"log"
+	"net"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/saurabh/protohackers/internal/logger"
+)
+
+var port = flag.String("port", "50001", "Port to listen on")
+
+func handleConnection(conn net.Conn) {
+	log.Println("New connection from", conn.RemoteAddr())
+	defer conn.Close()
+	defer log.Println("Connection closed from", conn.RemoteAddr())
+
+}
+
+func main() {
+	flag.Parse()
+
+	// Setup logging to logs directory
+	logFile, err := logger.Setup("unusual-db")
+	if err != nil {
+		panic(err)
+	}
+	defer logFile.Close()
+
+	ln, err := net.Listen("tcp", ":"+*port)
+	if err != nil {
+		panic(err)
+	}
+	defer ln.Close()
+
+	log.Println("Listening on port " + *port)
+
+	// Setup context for graceful shutdown
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	// Handle signals for graceful shutdown
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, os.Interrupt, syscall.SIGTERM)
+
+	go func() {
+		<-sigChan
+		log.Println("Shutting down gracefully...")
+		cancel()
+		ln.Close()
+	}()
+
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			select {
+			case <-ctx.Done():
+				log.Println("Server stopped")
+				return
+			default:
+				log.Println("Accept error:", err)
+				continue
+			}
+		}
+		go handleConnection(conn)
+	}
+}
